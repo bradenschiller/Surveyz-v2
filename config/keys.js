6a3dd5fef989828d7e58b7f596e46309348e6800
bradenschiller17 <bradenschiller17@gmail.com>
2018-02-15 13:59:45 -0600

    refactored files

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+//add this file to git ignore
+
+module.exports = {
+  google: {
+    clientID:
+      "255585321730-65icqdsoi3mtuc8k97dgfsqst9la2993.apps.googleusercontent.com",
+    clientSecret: "uSJKXAjVShA9VPR7D8kZLfhg"
+  }
+};
---
+++ services/passport.js
@@ -0,0 +1,18 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("../config/keys");
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.google.clientID,
+      clientSecret: keys.google.clientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("accessToken:", accessToken);
+      console.log("refreshToken:", refreshToken);
+      console.log("profile:", profile);
+    }
+  )
+);
