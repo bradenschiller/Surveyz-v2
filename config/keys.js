6dbff094659ffcc64e38d25d8e8de6201a1c70b4
bradenschiller17 <bradenschiller17@gmail.com>
2018-02-15 14:47:51 -0600

    hide keys file

--- config/keys.js
+++
@@ -1,9 +0,0 @@
-//add this file to git ignore
-
-module.exports = {
-  google: {
-    clientID:
-      "255585321730-65icqdsoi3mtuc8k97dgfsqst9la2993.apps.googleusercontent.com",
-    clientSecret: "uSJKXAjVShA9VPR7D8kZLfhg"
-  }
-};
